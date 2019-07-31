099a52c10ce82d29d4c6d11c3e64ef0dacd6de94
Maiara Barroso <maiarabarroso84@gmail.com>
2019-07-31 17:20:58 +0100

    Added final functionalities to the getFlights endpoint

--- api.go
+++ api.go
@@ -25,33 +25,61 @@ const (
 )
 
 func getFlights(w http.ResponseWriter, r *http.Request) {
+	notify := w.(http.CloseNotifier).CloseNotify()
+	go func() {
+		<-notify
+		log.Println("The client closed the connection prematurely. Cleaning up.")
+		// panic(http.ErrAbortHandler)
+	}()
 
 	queryValues := r.URL.Query()
+	// roundTrip, _ := strconv.ParseBool(queryValues.Get("roundTrip"))
 	origin := queryValues.Get("origin")
 	destination := queryValues.Get("destination")
 	outboundWeekDay, _ := strconv.Atoi(queryValues.Get("outboundWeekDay"))
 	duration, _ := strconv.Atoi(queryValues.Get("duration"))
+	country := queryValues.Get("country")
+	currency := queryValues.Get("currency")
+	locale := queryValues.Get("locale")
 
 	intervals := getDateIntervals(time.Weekday(outboundWeekDay), duration, time.Now(), time.Now().AddDate(0, 3, 0))
 	data := url.Values{}
-	data.Set("cabinClass", "business")
-	data.Set("country", "UK")
-	data.Set("currency", "GBP")
-	data.Set("locale", "en-UK")
+	data.Set("cabinClass", "economy")
 	data.Set("adults", "1")
+	data.Set("locale", locale)
+	data.Set("currency", currency)
+	data.Set("country", country)
 	data.Set("originPlace", origin)
 	data.Set("destinationPlace", destination)
 
-	var flightResults []FlightsResult
 	var counter = 0
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, HEAD, GET")
+	// w.Header().Set("Connection", "Keep-Alive")
+	// w.Header().Set("Transfer-Encoding", "chunked")
+	w.WriteHeader(http.StatusOK)
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		log.Printf("Expected http.ResponseWriter to be an http.Flusher")
+
+	}
+	w.Write([]byte(fmt.Sprintf("%d<", len(intervals))))
+	flusher.Flush()
+
 	for i := 0; i < len(intervals); i++ {
 		data.Set("outboundDate", intervals[i].Outbound.Format(layoutISO))
 		data.Set("inboundDate", intervals[i].Inbound.Format(layoutISO))
 
 		flightResponse := getItineraries(data)
+		var flightResults []FlightsResult
 
 		for _, it := range flightResponse.Itineraries {
 			result := FlightsResult{}
+
+			// Setting result Currency
+			result.Currency = searchCurrencyByCode(flightResponse.Currencies, flightResponse.Query.Currency)
 
 			// Setting result AgentInfo
 			var agentsInfo []AgentInfo
@@ -68,28 +96,31 @@ func getFlights(w http.ResponseWriter, r *http.Request) {
 			result.OutboundLeg = configResponseLeg(flightResponse, it.OutboundLegID)
 
 			flightResults = append(flightResults, result)
+
 			// log.Println("RESULT: ", result)
-			counter += 1
-			log.Println("Itinaerary #", counter)
+			counter++
+			log.Println("Itinerary #", counter)
 		}
-	}
-	log.Println("#results: ", counter)
 
-	flightResultJSON, err := json.Marshal(flightResults)
-	if err != nil {
-		fmt.Printf("The response marshalling failed with error %s\n", err)
-	}
+		// json.NewEncoder(w).Encode(flightResults)
+		flightResultJSON, err := json.Marshal(flightResults)
+		if err != nil {
+			fmt.Printf("The response marshalling failed with error %s\n", err)
+		}
 
-	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	w.WriteHeader(http.StatusOK)
-	w.Write(flightResultJSON)
+		w.Write(flightResultJSON)
+		w.Write([]byte("<"))
+		flusher.Flush()
+	}
+	//log.Println("#results: ", counter)
+
 }
 
 func configResponseLeg(flightResponse FlightsResponse, legID string) LegResponse {
 
 	leg := searchLegByID(flightResponse.Legs, legID)
 
-	var stops []PlaceResponse
+	stops := []PlaceResponse{}
 	for _, stopID := range leg.StopsIds {
 		stop := searchPlaceByID(flightResponse.Places, stopID)
 		stops = append(stops, PlaceResponse{stop.Name, stop.Code})
@@ -98,13 +129,13 @@ func configResponseLeg(flightResponse FlightsResponse, legID string) LegResponse
 	origin := searchPlaceByID(flightResponse.Places, leg.OriginStation)
 	destination := searchPlaceByID(flightResponse.Places, leg.DestinationStation)
 
-	var carriers []CarrierResponse
+	carriers := []CarrierResponse{}
 	for _, carrierID := range leg.CarrierIds {
 		carrier := searchCarrierByID(flightResponse.Carriers, carrierID)
 		carriers = append(carriers, CarrierResponse{carrier.Name, carrier.ImageURL})
 	}
 
-	var segs []SegmentResponse
+	segs := []SegmentResponse{}
 	for _, segID := range leg.SegmentIds {
 		seg := searchSegmentByID(flightResponse.Segments, segID)
 		origin := searchPlaceByID(flightResponse.Places, seg.OriginStation)
@@ -118,11 +149,14 @@ func configResponseLeg(flightResponse FlightsResponse, legID string) LegResponse
 		segs = append(segs, segResponse)
 	}
 
-	return LegResponse{leg.Departure, leg.Arrival, leg.Duration, stops, origin.Code, destination.Code,
+	return LegResponse{leg.Departure, leg.Arrival, leg.Duration, stops,
+		PlaceResponse{origin.Name, origin.Code},
+		PlaceResponse{destination.Name, destination.Code},
 		carriers, segs}
 }
 
 type FlightsResult struct {
+	Currency    Currency
 	AgentsInfo  []AgentInfo
 	InboundLeg  LegResponse
 	OutboundLeg LegResponse
@@ -140,8 +174,8 @@ type LegResponse struct {
 	Arrival     MyTime
 	Duration    int
 	Stops       []PlaceResponse // place codes
-	Origin      string          // place code
-	Destination string          //place code
+	Origin      PlaceResponse   // place code
+	Destination PlaceResponse   //place code
 	Carriers    []CarrierResponse
 	Segments    []SegmentResponse
 }
