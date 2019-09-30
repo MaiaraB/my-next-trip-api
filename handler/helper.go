5308f323e41780e6cf4643c856b085e7131db9c9
Maiara Barroso <maiarabarroso84@gmail.com>
2019-09-30 14:01:55 +0100

    Fixed a bug on the dates of the search

--- handler/helper.go
+++ handler/helper.go
@@ -22,14 +22,14 @@ func getDateIntervals(weekday time.Weekday, duration int, beginDate time.Time, e
 	inbound := outbound.AddDate(0, 0, duration)
 
 	for {
-		if inbound.Before(endDate) {
+		if outbound.Before(endDate) || outbound.Equal(endDate) {
 			intervals = append(intervals, Interval{Outbound: outbound, Inbound: inbound})
+			log.Println("OUTBOUND: ", outbound, " / INBOUND: ", inbound)
 		} else {
 			break
 		}
 		outbound = outbound.AddDate(0, 0, 7)
 		inbound = outbound.AddDate(0, 0, duration)
-		log.Println("OUTBOUND: ", outbound, " / INBOUND: ", inbound)
 	}
 
 	return intervals
